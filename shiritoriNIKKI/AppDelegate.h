102124c58a09c9a554b0f63d550c7f2ed2959daf
OSHIMAYOSHIAKI <oshimayoshiaki@OSHIMAYOSHIAKI-no-MacBook-Air.local>
2013-10-09 16:36:41 +0900

    Initial Commit

---
+++ shiritoriNIKKI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shiritoriNIKKI
+//
+//  Created by OSHIMAYOSHIAKI on 2013/10/09.
+//  Copyright (c) 2013年 OSHIMAYOSHIAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shiritoriNIKKI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  shiritoriNIKKI
+//
+//  Created by OSHIMAYOSHIAKI on 2013/10/09.
+//  Copyright (c) 2013年 OSHIMAYOSHIAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
